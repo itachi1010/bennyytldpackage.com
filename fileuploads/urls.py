d77c0e994e312c2d1086856e82616702f9cd8f5c
itachi1010 <onahben977@gmail.com>
2023-11-13 11:40:30 -1100

    i am adding a navbar

--- fileuploads/urls.py
+++ fileuploads/urls.py
@@ -3,6 +3,8 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
+
+
 urlpatterns = [
     path('upload/', views.upload_file, name='upload_file'),
     path('download/<int:file_id>/', views.download_file, name='download_file'),
